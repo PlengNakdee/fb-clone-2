51d686b813c8963ec5a4a6ebd12796ef3e61d07b
Pleng Nakdee <plengwn@gmail.com>
2021-05-17 00:04:38 +0700

    checkpoint

--- components/Header.js
+++ components/Header.js
@@ -1,10 +1,21 @@
 import Image from "next/image";
+import HeaderIcon from "./HeaderIcon";
 import { SearchIcon } from "@heroicons/react/outline";
-import { BellIcon } from "@heroicons/react/solid";
+import {
+  BellIcon,
+  HomeIcon,
+  FlagIcon,
+  PlayIcon,
+  ShoppingCartIcon,
+  UserGroupIcon,
+} from "@heroicons/react/solid";
 
 function Header() {
   return (
-    <div>
+    <div
+      className="sticky top-0 z-50 bg-white flex
+    items-center p-2 lg:px-5 shadow-md"
+    >
       <div className="flex items-center">
         <Image
           src="/f_logo_RGB-Blue_72.png"
@@ -12,13 +23,23 @@ function Header() {
           height={40}
           layout="fixed"
         />
-        <div className="flex ml-2 items-center rounded-full">
-          <SearchIcon className="h-6" />
-          <input type="text" placeholder="Search Facebook" />
+        <div className="flex ml-2 items-center rounded-full bg-gray-100 p-2">
+          <SearchIcon className="h-6 text-gray-600" />
+          <input
+            className="flex ml-2 items-center bg-transparent outline-none flex-shrink"
+            type="text"
+            placeholder="Search Facebook"
+          />
         </div>
       </div>
-      <div>
-        <h1>middle</h1>
+      <div className="flex justify-center flex-grow">
+        <div className="flex space-x-6 md:space-x-2">
+          <HeaderIcon Icon={HomeIcon} />
+          <HeaderIcon Icon={FlagIcon} />
+          <HeaderIcon Icon={PlayIcon} />
+          <HeaderIcon Icon={ShoppingCartIcon} />
+          <HeaderIcon Icon={UserGroupIcon} />
+        </div>
       </div>
       <div>
         <h1>right</h1>
---
+++ components/HeaderIcon.js
@@ -0,0 +1,15 @@
+function HeaderIcon({ Icon }) {
+  return (
+    <div
+      className="flex items-center cursor-pointer sm:h-14 md:px-10
+    md:hover:bg-gray-100 rounded-xl active:border-b-2 active:border-blue-500 group"
+    >
+      <Icon
+        className="h-5 group-hover:text-blue-500 text-center
+      sm:h-7 mx-auto"
+      />
+    </div>
+  );
+}
+
+export default HeaderIcon;
